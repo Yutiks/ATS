a2b3a803e1094b1e4b2e4c12b9e81e1853ab683a
YUTIKS <yutiks15@gmail.com>
2026-07-07 14:34:40 +0100

    add Footer&Navbar and MainLayout

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>© 2026 InkFlow Tattoo Studio</p>
+        </footer>
+    );
+}
+
+export default Footer;
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Artists from "../pages/Artists";
@@ -18,19 +18,21 @@ function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/artists" element={<Artists />} />
-                <Route path="/artist" element={<Artist />} />
-                <Route path="/portfolio" element={<Portfolio />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/pricing" element={<Pricing />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/post" element={<Post />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/profile" element={<Profile />} />
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/artists" element={<Artists />} />
+                    <Route path="/artist" element={<Artist />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/pricing" element={<Pricing />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/post" element={<Post />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
